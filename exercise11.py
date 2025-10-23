64907027d9d1ac53fb6d337f781b2869c08094c8
Steeven Lopez <steeeven.p.2002@gmail.com>
2025-10-23 20:30:17 +0200

    terminar

--- exercise11.py
+++ exercise11.py
@@ -1,17 +1,20 @@
+##Creo la clase student
 class student:
+    #Atributos name y grade rivados
     def __init__(self,grade,name):
         self.__name=name
         self.__grade=grade
-    def set_grade(self):
-        if 0 <= student.__grade <= 10:
-            self.__grade
+    ##En este seter comprobamos que la nota este entre 0 y 10 
+    def set_grade(self,newGrade):
+        if 0 <= newGrade <= 10:
+            self.__grade=newGrade
         else:
-            print("La nota no esta en el rango permitido")
+            print( "La nota no esta en el rango permitido")
     def get_grade(self):
-        return self.__grade
+        return self.grade
     def promote (self):
-        while student.__grade< 10:
-            self.__grade=self.__grade+1
-
-
-    
+        if self.grade< 10:
+            self.grade=self.grade+1
+    def __eq__(self):
+        print("")
+##creamos los objetos?
